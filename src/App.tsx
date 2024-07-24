cd7206042b95d4bd09d0a9fa720e6fd1fd3a58cb
Philip Indra Prayitno <philip.prayitno@gmail.com>
2024-07-24 23:21:48 +0700

    [Feature] Question 2 ANswer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,45 @@ import "@fontsource/roboto/700.css";
 import Form from "./Form";
 
 const App = () => {
-  return <Form/>
-}
+  const calculateMinMax = (values: number[][]) => {
+    let minimum = 0;
+    for (let i = 0; i < values.length; i++) {
+      // I Assume the sub array, might not always be sorted
+      // Therefore i will sort descending to get the lowest value of each array in the first index
+      // to save some time
+      const sortedArray = values[i].sort((a, b) => a - b);
+
+      // Always set the minimum to the first array
+      if (i === 0) {
+        minimum = sortedArray[0];
+      } else if(sortedArray[0] < minimum) {
+        minimum = sortedArray[0]
+      }
+    }
+      
+    return minimum;
+  };
+
+  const values: number[][] = [
+    [1, 2, 3, 4, 5],
+    [3, 4, 5, 6, 7],
+    [6, 7, 8, 9, 10],
+  ];
+
+  
+  const values2: number[][] = [
+    [13, 2, 8, 9, 1],
+    [-1, 0, 100, 20, -50],
+    [0, 0, 0],
+  ];
+
+  const answer: number = calculateMinMax(values);
+  const answer2: number = calculateMinMax(values2);
+
+  console.log("The result of first test case is " + answer);
+  console.log("The result of second test case is " + answer2);
+
+  return <Form />;
+};
 
 export default App;
